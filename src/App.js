b274dbee15d978b2594507095f58753d17808ecd
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-05-13 18:45:45 +1000

    Created app /w basic imports, state and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// API Request
+import axios from 'axios';
+
+// Navigation
+import NavBar from './components/NavBar.js';
+
+// Page Content
+import Home from './components/Home';
+import Quiz from './components/Quiz';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+// 404 Page
+import NotFound from './components/404';
+
+// Styling
 import './App.css';
 
-function App() {
+export default function App() {
+  const [loginState, setLoginState] = useState({
+    isLoggedIn: false,
+    user: '',
+    userID: '',
+  });
+
+  // Axios GET request to API method to check if there is a logged in user
+  const loginStatus = () => {
+    axios
+      .get(process.env.REACT_APP_DOMAIN + '/logged_in', { withCredentials: true })
+      .then((response) => {
+        if (response.data.logged_in) {
+          handleLogin(response.data.user);
+        } else {
+          handleLogout();
+        }
+      })
+      .catch((error) => console.log('api errors:', error));
+  };
+
+  // Sets isLoggedIn to True and user to user data received from Rails
+  const handleLogin = (data) => {
+    setLoginState((prevState) => ({
+      ...prevState,
+      isLoggedIn: true,
+      user: data.username,
+      userID: data.id,
+    }));
+    // setIsLoggedIn(true);
+    // setUserID(data.id);
+    // setUser(data.username);
+  };
+
+  // Sets isLoggedIn to False and clears current user data
+  const handleLogout = () => {
+    setLoginState((prevState) => ({
+      ...prevState,
+      isLoggedIn: false,
+      user: '',
+      userID: '',
+    }));
+    // setIsLoggedIn(false);
+    // setUserID(data.id);
+    // setUser('');
+  };
+
+  // On re-render, check loginStatus
+  useEffect(loginStatus);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="main-container">
+          <NavBar isLoggedIn={loginState.isLoggedIn} isQuizPage={false} />
+
+          <div className="content-container">
+            <Switch>
+              <Route
+                exact
+                path="/signup"
+                render={(props) => <SignUp {...props} handleLogin={handleLogin} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={(props) => <Login {...props} handleLogin={handleLogin} />}
+              />
+              <Route exact path="/quiz" component={Quiz} />
+              <Route exact path="/" component={Home} />
+
+              {/* URL with no matching route calls the 404 component */}
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
-
-export default App;
