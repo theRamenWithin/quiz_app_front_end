808d83704283e7b88b6c359d0471aa1cdfa0e88c
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-05-16 21:08:40 +1000

    useHistory to get to quiz. Conditional content.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function Home({ getQuiz }) {
+export default function Home({ isLoggedIn, getQuiz }) {
+  let history = useHistory();
+
   const [values, setValues] = useState({
     numberOfQs: 5,
     difficulty: 'medium',
@@ -14,6 +17,7 @@ export default function Home({ getQuiz }) {
   const submitHandler = (e) => {
     e.preventDefault();
     getQuiz(values.numberOfQs, values.difficulty);
+    history.push('./quiz');
   };
 
   return (
@@ -21,31 +25,35 @@ export default function Home({ getQuiz }) {
       <h1>Quiz App</h1>
       <h2>by Alex Pike</h2>
 
-      <form onSubmit={submitHandler}>
-        <div>
-          <label>Number of Questions:</label>
-          <input
-            type="number"
-            min="3"
-            max="8"
-            step="1"
-            name="numberOfQs"
-            value={values.numberOfQs}
-            onChange={inputChangeHandler}
-          />
-        </div>
-        <div>
-          <label>Difficulty:</label>
-          <select name="difficulty" value="medium" onChange={inputChangeHandler}>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-        </div>
-        <div>
-          <button type="submit">Start Quiz!</button>
-        </div>
-      </form>
+      {isLoggedIn ? (
+        <form onSubmit={submitHandler}>
+          <div>
+            <label>Number of Questions:</label>
+            <input
+              type="number"
+              min="3"
+              max="8"
+              step="1"
+              name="numberOfQs"
+              value={values.numberOfQs}
+              onChange={inputChangeHandler}
+            />
+          </div>
+          <div>
+            <label>Difficulty:</label>
+            <select name="difficulty" value="medium" onChange={inputChangeHandler}>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </div>
+          <div>
+            <button type="submit">Start Quiz!</button>
+          </div>
+        </form>
+      ) : (
+        <p>Please login to start a new quiz</p>
+      )}
     </>
   );
 }
