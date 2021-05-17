5b127d8f8493d692e28e19e54251c35c941f2a3e
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-05-17 18:34:07 +1000

    Created basic Results page output

---
+++ src/components/Result.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Result({ quizResult }) {
+  console.log(quizResult);
+
+  return (
+    <div>
+      <h1>Your quiz result: {quizResult.outcome}</h1>
+      <h2>
+        Quiz difficulty was:{' '}
+        {quizResult.difficulty.charAt(0).toUpperCase() + quizResult.difficulty.slice(1)}
+      </h2>
+      <h2>
+        You answered {quizResult.numOfCorrectQs} correctly out of {quizResult.numOfQs}.
+      </h2>
+
+      {quizResult.outcome === 'Pass' ? (
+        <p>
+          Well done.{quizResult.difficulty !== 'hard' ? ' Why not try a harder difficulty?' : null}
+        </p>
+      ) : (
+        <p>Too bad. Why not try again?</p>
+      )}
+
+      <Link to={'/'} className="nav-link">
+        Back to Home
+      </Link>
+    </div>
+  );
+}
